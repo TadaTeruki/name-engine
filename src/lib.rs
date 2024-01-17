22d5e563d4974ba1c5eae30f13d9e209a25ad18c
Teruki TADA <tadateruki.public@gmail.com>
2024-01-17 13:04:23 +0900

    3文字地名の有効化

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 type Kanji = String;
 type Romaji = String;
+type ToRestore = bool;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Ascii {
@@ -57,7 +58,7 @@ impl PhonicsGraphBuilder {
 
     fn build(self) -> PhonicsGraph {
         PhonicsGraph { graph: self.graph }
-    }   
+    }
 }
 
 struct PhonicsGraph {
@@ -66,11 +67,11 @@ struct PhonicsGraph {
 }
 
 impl PhonicsGraph {
-    fn extract_forward(&self, ascii: Ascii, prop: f64) -> Ascii {
+    fn extract_forward(&self, ascii: Ascii, index: usize) -> Ascii {
         let code = ascii.to_code() as usize;
         let outgoing_items = &self.graph[code].0;
         let outgoing_sum = self.graph[code].1;
-        let to_extract = (outgoing_sum as f64 * prop) as usize;
+        let to_extract = index % outgoing_sum;
         let mut sum = 0;
         for item in outgoing_items {
             sum += item.1;
@@ -83,20 +84,16 @@ impl PhonicsGraph {
 }
 
 pub struct PlaceName {
-    pub incoming_phrase: (Kanji, Romaji),
-    pub outgoing_phrase: (Kanji, Romaji),
+    pub phrases: Vec<(Kanji, Romaji)>,
 }
 
 impl PlaceName {
-    pub fn new(
-        incoming_kanji: &str,
-        incoming_romaji: &str,
-        outgoing_kanji: &str,
-        outgoing_romaji: &str,
-    ) -> Self {
+    pub fn new(phrases: Vec<(&str, &str)>) -> Self {
         Self {
-            incoming_phrase: (incoming_kanji.to_string(), incoming_romaji.to_string()),
-            outgoing_phrase: (outgoing_kanji.to_string(), outgoing_romaji.to_string()),
+            phrases: phrases
+                .iter()
+                .map(|(k, r)| (k.to_string(), r.to_string()))
+                .collect(),
         }
     }
 }
@@ -105,13 +102,19 @@ pub struct PlaceNameGeneratorBuilder {
     place_names: Vec<PlaceName>,
 }
 
+impl Default for PlaceNameGeneratorBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl PlaceNameGeneratorBuilder {
     pub fn new() -> Self {
         Self {
             place_names: vec![],
         }
     }
-    
+
     pub fn add_place_name(mut self, place_name: PlaceName) -> Self {
         self.place_names.push(place_name);
         self
@@ -129,17 +132,37 @@ impl PlaceNameGeneratorBuilder {
             .collect::<Vec<(Ascii, Vec<usize>)>>();
         let mut incoming_phrases = vec![];
         let mut outgoing_phrases = vec![];
-        self.place_names.iter().enumerate().for_each(|(i, place_name)| {
-            if let (Some(incoming_char), Some(outgoing_char)) = (place_name.incoming_phrase.1.chars().last(), place_name.outgoing_phrase.1.chars().next()) {
-                let incoming_ascii = Ascii::from_char(incoming_char);
-                let outgoing_ascii = Ascii::from_char(outgoing_char);
+        self.place_names.iter().for_each(|place_name| {
+            for i in 0..place_name.phrases.len() - 1 {
+                let (incoming_char, outgoing_char) = (
+                    place_name.phrases[i].1.chars().last(),
+                    place_name.phrases[i + 1].1.chars().next(),
+                );
+                if incoming_char.is_none() || outgoing_char.is_none() {
+                    continue;
+                }
+                let (incoming_char, outgoing_char) =
+                    (incoming_char.unwrap(), outgoing_char.unwrap());
+                let (incoming_ascii, outgoing_ascii) = (
+                    Ascii::from_char(incoming_char),
+                    Ascii::from_char(outgoing_char),
+                );
                 graph_builder.add_ascii_pair(incoming_ascii, outgoing_ascii);
-                outgoing_tree[outgoing_ascii.to_code() as usize].1.push(i);
-                incoming_phrases.push(place_name.incoming_phrase.clone());
-                outgoing_phrases.push(place_name.outgoing_phrase.clone());
+                if i == 0 {
+                    incoming_phrases.push(place_name.phrases[i].clone());
+                }
+                let to_restore = i + 1 != place_name.phrases.len() - 1;
+                outgoing_phrases.push((
+                    place_name.phrases[i + 1].0.clone(),
+                    place_name.phrases[i + 1].1.clone(),
+                    to_restore,
+                ));
+                outgoing_tree[outgoing_ascii.to_code() as usize]
+                    .1
+                    .push(outgoing_phrases.len() - 1);
             }
         });
-        
+
         PlaceNameGenerator {
             incoming_phrases,
             outgoing_phrases,
@@ -153,7 +176,7 @@ pub struct PlaceNameGenerator {
     // PlaceNameのincomingを格納する木構造
     incoming_phrases: Vec<(Kanji, Romaji)>,
     // PlaceNameのoutgoingを格納する木構造
-    outgoing_phrases: Vec<(Kanji, Romaji)>,
+    outgoing_phrases: Vec<(Kanji, Romaji, ToRestore)>,
     // 各Phonicsから始まるoutgoing phraseのindexを格納する木構造
     outgoing_tree: Vec<(Ascii, Vec<usize>)>,
     // Phonics同士の接続を表すグラフ
@@ -161,24 +184,32 @@ pub struct PlaceNameGenerator {
 }
 
 impl PlaceNameGenerator {
+    pub fn generate(&self, mut pfunc: impl FnMut() -> usize) -> (Kanji, Romaji) {
+        let append = |incoming_phrase: (Kanji, Romaji), p0: usize, p1: usize| {
+            let connection_phrase = self.graph.extract_forward(
+                Ascii::from_char(incoming_phrase.1.chars().last().unwrap()),
+                p0,
+            );
 
-    pub fn generate(&self, p0: f64, p1: f64, p2: f64) -> (Kanji, Romaji) {
-        let incoming_phrase_code = (self.incoming_phrases.len() as f64 * p0) as usize;
+            let outgoing_phrase_list = &self.outgoing_tree[connection_phrase.to_code() as usize];
+            let outgoing_phrase_code = p1 % outgoing_phrase_list.1.len();
+            let outgoing_phrase =
+                self.outgoing_phrases[outgoing_phrase_list.1[outgoing_phrase_code]].clone();
+            (
+                incoming_phrase.0 + &outgoing_phrase.0,
+                incoming_phrase.1 + &outgoing_phrase.1,
+                outgoing_phrase.2,
+            )
+        };
+
+        let incoming_phrase_code = pfunc() % self.incoming_phrases.len();
         let incoming_phrase = self.incoming_phrases[incoming_phrase_code].clone();
-        let connection_phrase = self.graph.extract_forward(
-            Ascii::from_char(incoming_phrase.1.chars().last().unwrap()),
-            p1,
-        );
-        let outgoing_phrase_code = (self.outgoing_tree[connection_phrase.to_code() as usize]
-            .1
-            .len() as f64
-            * p2) as usize;
-        let outgoing_phrase = self.outgoing_phrases
-            [self.outgoing_tree[connection_phrase.to_code() as usize].1[outgoing_phrase_code]]
-            .clone();
-        (
-            incoming_phrase.0 + &outgoing_phrase.0,
-            incoming_phrase.1 + &outgoing_phrase.1,
-        )
+
+        let mut incoming_phrase = append(incoming_phrase, pfunc(), pfunc());
+        while incoming_phrase.2 {
+            incoming_phrase = append((incoming_phrase.0, incoming_phrase.1), pfunc(), pfunc());
+        }
+
+        (incoming_phrase.0, incoming_phrase.1)
     }
 }
--- tests/prototype.rs
+++ tests/prototype.rs
@@ -5,144 +5,143 @@ use rand::{rngs::StdRng, Rng, SeedableRng};
 fn test_simple_generation() {
     let place_names = vec![
         // 青森
-        PlaceName::new("青", "ao", "森", "mori"),
+        PlaceName::new(vec![("青", "ao"), ("森", "mori")]),
         // 盛岡
-        PlaceName::new("盛", "mori", "岡", "oka"),
+        PlaceName::new(vec![("盛", "mori"), ("岡", "oka")]),
         // 酒田
-        PlaceName::new("酒", "saka", "田", "ta"),
+        PlaceName::new(vec![("酒", "saka"), ("田", "ta")]),
         // 米沢
-        PlaceName::new("米", "yone", "沢", "zawa"),
+        PlaceName::new(vec![("米", "yone"), ("沢", "zawa")]),
         // 秋田
-        PlaceName::new("秋", "aki", "田", "ta"),
+        PlaceName::new(vec![("秋", "aki"), ("田", "ta")]),
         // 山形
-        PlaceName::new("山", "yama", "形", "gata"),
+        PlaceName::new(vec![("山", "yama"), ("形", "gata")]),
         // 福島
-        PlaceName::new("福", "fuku", "島", "shima"),
+        PlaceName::new(vec![("福", "fuku"), ("島", "shima")]),
         // 仙台
-        PlaceName::new("仙", "sen", "台", "dai"),
+        PlaceName::new(vec![("仙", "sen"), ("台", "dai")]),
         // 長岡
-        PlaceName::new("長", "naga", "岡", "oka"),
+        PlaceName::new(vec![("長", "naga"), ("岡", "oka")]),
         // 上越
-        PlaceName::new("上", "jo", "越", "etsu"),
+        PlaceName::new(vec![("上", "jo"), ("越", "etsu")]),
         // 佐渡
-        PlaceName::new("佐", "sa", "渡", "do"),
+        PlaceName::new(vec![("佐", "sa"), ("渡", "do")]),
         // 高崎
-        PlaceName::new("高", "taka", "崎", "saki"),
+        PlaceName::new(vec![("高", "taka"), ("崎", "saki")]),
         // 日立
-        PlaceName::new("日", "hi", "立", "tachi"),
+        PlaceName::new(vec![("日", "hi"), ("立", "tachi")]),
         // 水戸
-        PlaceName::new("水", "mi", "戸", "to"),
+        PlaceName::new(vec![("水", "mi"), ("戸", "to")]),
         // 成田
-        PlaceName::new("成", "nari", "田", "ta"),
+        PlaceName::new(vec![("成", "nari"), ("田", "ta")]),
         // 佐倉
-        PlaceName::new("佐", "sa", "倉", "kura"),
+        PlaceName::new(vec![("佐", "sa"), ("倉", "kura")]),
         // 市原
-        PlaceName::new("市", "ichi", "原", "hara"),
+        PlaceName::new(vec![("市", "ichi"), ("原", "hara")]),
         // 立川
-        PlaceName::new("立", "tachi", "川", "kawa"),
+        PlaceName::new(vec![("立", "tachi"), ("川", "kawa")]),
         // 所沢
-        PlaceName::new("所", "tokoro", "沢", "zawa"),
+        PlaceName::new(vec![("所", "tokoro"), ("沢", "zawa")]),
         // 飯田
-        PlaceName::new("飯", "ii", "田", "da"),
+        PlaceName::new(vec![("飯", "ii"), ("田", "da")]),
         // 長野
-        PlaceName::new("長", "naga", "野", "no"),
+        PlaceName::new(vec![("長", "naga"), ("野", "no")]),
         // 松本
-        PlaceName::new("松", "matsu", "本", "moto"),
+        PlaceName::new(vec![("松", "matsu"), ("本", "moto")]),
         // 豊橋
-        PlaceName::new("豊", "toyo", "橋", "hashi"),
+        PlaceName::new(vec![("豊", "toyo"), ("橋", "hashi")]),
         // 岡崎
-        PlaceName::new("岡", "oka", "崎", "zaki"),
+        PlaceName::new(vec![("岡", "oka"), ("崎", "zaki")]),
         // 豊田
-        PlaceName::new("豊", "toyo", "田", "ta"),
+        PlaceName::new(vec![("豊", "toyo"), ("田", "ta")]),
         // 高山
-        PlaceName::new("高", "taka", "山", "yama"),
+        PlaceName::new(vec![("高", "taka"), ("山", "yama")]),
         // 金沢
-        PlaceName::new("金", "kana", "沢", "zawa"),
+        PlaceName::new(vec![("金", "kana"), ("沢", "zawa")]),
         // 富山
-        PlaceName::new("富", "to", "山", "yama"),
+        PlaceName::new(vec![("富", "to"), ("山", "yama")]),
         // 福井
-        PlaceName::new("福", "fuku", "井", "i"),
+        PlaceName::new(vec![("福", "fuku"), ("井", "i")]),
         // 甲府
-        PlaceName::new("甲", "ko", "府", "fu"),
+        PlaceName::new(vec![("甲", "ko"), ("府", "fu")]),
         // 静岡
-        PlaceName::new("静", "shizu", "岡", "oka"),
+        PlaceName::new(vec![("静", "shizu"), ("岡", "oka")]),
         // 浜松
-        PlaceName::new("浜", "hama", "松", "matsu"),
+        PlaceName::new(vec![("浜", "hama"), ("松", "matsu")]),
         // 長浜
-        PlaceName::new("長", "naga", "浜", "hama"),
+        PlaceName::new(vec![("長", "naga"), ("浜", "hama")]),
         // 小浜
-        PlaceName::new("小", "o", "浜", "bama"),
+        PlaceName::new(vec![("小", "o"), ("浜", "bama")]),
         // 大津
-        PlaceName::new("大", "o", "津", "tsu"),
+        PlaceName::new(vec![("大", "o"), ("津", "tsu")]),
         // 亀岡
-        PlaceName::new("亀", "kame", "岡", "oka"),
+        PlaceName::new(vec![("亀", "kame"), ("岡", "oka")]),
         // 豊岡
-        PlaceName::new("豊", "toyo", "岡", "oka"),
+        PlaceName::new(vec![("豊", "toyo"), ("岡", "oka")]),
         // 三田
-        PlaceName::new("三", "san", "田", "da"),
+        PlaceName::new(vec![("三", "san"), ("田", "da")]),
         // 松原
-        PlaceName::new("松", "matsu", "原", "bara"),
+        PlaceName::new(vec![("松", "matsu"), ("原", "bara")]),
         // 有田
-        PlaceName::new("有", "ari", "田", "ta"),
-        // 田辺
-        PlaceName::new("田", "ta", "辺", "nabe"),
+        PlaceName::new(vec![("有", "ari"), ("田", "ta")]),
         // 徳島
-        PlaceName::new("徳", "toku", "島", "shima"),
+        PlaceName::new(vec![("徳", "toku"), ("島", "shima")]),
         // 高松
-        PlaceName::new("高", "taka", "松", "matsu"),
+        PlaceName::new(vec![("高", "taka"), ("松", "matsu")]),
         // 松山
-        PlaceName::new("松", "matsu", "山", "yama"),
+        PlaceName::new(vec![("松", "matsu"), ("山", "yama")]),
         // 南国
-        PlaceName::new("南", "nan", "国", "koku"),
+        PlaceName::new(vec![("南", "nan"), ("国", "koku")]),
         // 岡山
-        PlaceName::new("岡", "oka", "山", "yama"),
+        PlaceName::new(vec![("岡", "oka"), ("山", "yama")]),
         // 広島
-        PlaceName::new("広", "hiro", "島", "shima"),
+        PlaceName::new(vec![("広", "hiro"), ("島", "shima")]),
         // 福山
-        PlaceName::new("福", "fuku", "山", "yama"),
+        PlaceName::new(vec![("福", "fuku"), ("山", "yama")]),
         // 赤磐
-        PlaceName::new("赤", "aka", "磐", "iwa"),
+        PlaceName::new(vec![("赤", "aka"), ("磐", "iwa")]),
         // 津山
-        PlaceName::new("津", "tsu", "山", "yama"),
+        PlaceName::new(vec![("津", "tsu"), ("山", "yama")]),
         // 岩国
-        PlaceName::new("岩", "iwa", "国", "kuni"),
+        PlaceName::new(vec![("岩", "iwa"), ("国", "kuni")]),
         // 山口
-        PlaceName::new("山", "yama", "口", "guchi"),
+        PlaceName::new(vec![("山", "yama"), ("口", "guchi")]),
         // 宇部
-        PlaceName::new("宇", "u", "部", "be"),
+        PlaceName::new(vec![("宇", "u"), ("部", "be")]),
         // 浜田
-        PlaceName::new("浜", "hama", "田", "da"),
+        PlaceName::new(vec![("浜", "hama"), ("田", "da")]),
         // 大田
-        PlaceName::new("大", "o", "田", "ta"),
+        PlaceName::new(vec![("大", "o"), ("田", "ta")]),
         // 松江
-        PlaceName::new("松", "matsu", "江", "e"),
+        PlaceName::new(vec![("松", "matsu"), ("江", "e")]),
         // 倉吉
-        PlaceName::new("倉", "kura", "吉", "yoshi"),
+        PlaceName::new(vec![("倉", "kura"), ("吉", "yoshi")]),
+        // 佐世保
+        PlaceName::new(vec![("佐", "sa"), ("世", "se"), ("保", "bo")]),
         // 福岡
-        PlaceName::new("福", "fuku", "岡", "oka"),
+        PlaceName::new(vec![("福", "fuku"), ("岡", "oka")]),
         // 大村
-        PlaceName::new("大", "o", "村", "mura"),
+        PlaceName::new(vec![("大", "o"), ("村", "mura")]),
         // 島原
-        PlaceName::new("島", "shima", "原", "bara"),
+        PlaceName::new(vec![("島", "shima"), ("原", "bara")]),
         // 中津
-        PlaceName::new("中", "naka", "津", "tsu"),
+        PlaceName::new(vec![("中", "naka"), ("津", "tsu")]),
         // 玉名
-        PlaceName::new("玉", "tama", "名", "na"),
+        PlaceName::new(vec![("玉", "tama"), ("名", "na")]),
         // 人吉
-        PlaceName::new("人", "hito", "吉", "yoshi"),
+        PlaceName::new(vec![("人", "hito"), ("吉", "yoshi")]),
         // 霧島
-        PlaceName::new("霧", "kiri", "島", "shima"),
+        PlaceName::new(vec![("霧", "kiri"), ("島", "shima")]),
         // 日置
-        PlaceName::new("日", "hi", "置", "oki"),
+        PlaceName::new(vec![("日", "hi"), ("置", "oki")]),
     ];
 
-    let generator = PlaceNameGeneratorBuilder::new().bulk_add_place_names(place_names).build();
+    let generator = PlaceNameGeneratorBuilder::new()
+        .bulk_add_place_names(place_names)
+        .build();
     let mut rng = StdRng::seed_from_u64(0);
-    (0..50).for_each(|_| {
-        let props = (0..3)
-            .map(|_| rng.gen_range(0.0..1.0))
-            .collect::<Vec<f64>>();
-        let name = generator.generate(props[0], props[1], props[2]);
+    (0..10000).for_each(|_| {
+        let name = generator.generate(|| rng.gen());
         println!("{} {}", name.0, name.1);
     });
 }
