90e5390240b7af906d169ebefd84727d0801726f
Teruki TADA <me@peruki.dev>
2024-02-10 17:38:34 +0900

    add english

---
+++ examples/english.rs
@@ -0,0 +1,43 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+use placename_engine::{PlaceName, PlaceNameGeneratorBuilder};
+use rand::{rngs::StdRng, Rng, SeedableRng};
+
+fn format(name: String) -> String {
+    let name = name.replace('+', " ");
+    name
+}
+
+fn main() {
+    let csv_file = include_str!("assets/english.csv");
+
+    let place_names = csv_file
+        .lines()
+        .map(|line| {
+            let split = line.split(',');
+            let phrases = split
+                .last()
+                .unwrap()
+                .split(':')
+                .map(|phrase| {
+                    let mut split = phrase.split('_');
+                    (split.next().unwrap(), split.next().unwrap())
+                })
+                .collect::<Vec<(&str, &str)>>();
+            PlaceName::new(phrases)
+        })
+        .collect::<Vec<PlaceName>>();
+
+    let generator = PlaceNameGeneratorBuilder::new()
+        .bulk_add_place_names(place_names)
+        .build();
+    let mut rng: StdRng = SeedableRng::seed_from_u64(0);
+    (0..800).for_each(|_| {
+        let name = generator.generate(|| rng.gen());
+        println!("{} {}", format(name.0), format(name.1));
+    });
+}
